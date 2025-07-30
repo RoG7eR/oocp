e69aca68817d3602acc176861d3b1cfc7ff56243
RoG7eR <174163321+RoG7eR@users.noreply.github.com>
2025-07-30 10:54:38 +0530

    Initial commit 2

--- 2.0/2.2/defaultargu.cpp
+++ 2.0/2.2/defaultargu.cpp
@@ -1,19 +1,27 @@
 /* Write a program to demonstrate the use of default arguments in
 function overloading.*/
 
-#include <iostream>
+#include<iostream>
+#define PI 3.14
 using namespace std;
 
-int& getElement(int arr[], int index);
-
+void AOC(float r = 3.6);
+void AOR(int l = 5, int b = 6);
+void AOS(int l = 5);
 int main() {
-    int numbers[5] = {10, 20, 30, 40, 50};
-    cout << "Original value at index 2: " << numbers[2] << endl;
-    getElement(numbers, 2) = 100;
-    cout << "Updated value at index 2: " << numbers[2] << endl;
-    return 0;
+    AOC();
+    AOR();
+    AOS();
 }
 
-int& getElement(int arr[], int index) {
-    return arr[index]; 
+void AOC(float r) {
+    cout << PI * r * r << endl;
+}
+
+void AOR(int l, int b) {
+    cout << l * b << endl;
+}
+
+void AOS(int l = 5) {
+    cout << l * l;
 }
